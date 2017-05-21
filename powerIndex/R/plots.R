436ed589c10d9661a0486a2bc7256924563dd405
pecu <pecu610@gmail.com>
2017-05-21 10:44:41 +0800

    add plot funcion

--- powerIndex/R/plots.R
+++ powerIndex/R/plots.R
@@ -1,6 +1,11 @@
 plotPower <- function(buildingsPower)
 {
   library(ggplot2)
-    
-  
+  #load("./powerIndex/R/power.RData")  
+  #id = c(1,2,5)
+  #buildingsPower = power[1:24,id]
+  ggplot(data=buildingsPower, 
+         aes(x=buildingsPower[,2], 
+             y=buildingsPower[,3], group=1)) +
+    geom_line()
 }
